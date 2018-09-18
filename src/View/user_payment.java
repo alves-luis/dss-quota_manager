a5f68a4e3e94e71e4745939f8b5d9abc9a19306d
Recksun <diogo.goncaalves@gmail.com>
2018-09-18 23:32:17 +0100

    View update

---
+++ src/View/user_payment.java
@@ -0,0 +1,11 @@
+package View;
+
+import javax.swing.*;
+
+public class register_user {
+    private JButton button;
+    private JFormattedTextField input_value;
+    private JLabel number;
+    private JFormattedTextField input_number;
+    private JLabel value;
+}
